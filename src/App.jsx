34f92e1f9e2920540f74a288937b203ba6bda79c
Brenda Aguilar <blenda916@gmail.com>
2023-12-11 13:34:13 -0600

    Componente CalculationResult

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SerieForm from './components/NumberInputForm.jsx'
+import CalculationResult from './components/CalculationResult.jsx';
 import './App.css'
 
 function App() {
-
   return (
     <>
+      <h1>Serie(n)</h1>
       <SerieForm />
+      <CalculationResult />
     </>
   )
 }
---
+++ src/components/CalculationResult.jsx
@@ -0,0 +1,8 @@
+export default function CalculationResult(){
+    return(
+        <>
+            <h2>Resultado</h2>
+            <p>*****</p>
+        </>
+    )
+}
