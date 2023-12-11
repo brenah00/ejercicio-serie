16af15cf4750e64fbbc0675abe305e7eef70f69d
Brenda Aguilar <blenda916@gmail.com>
2023-12-11 12:33:15 -0600

    Componente SerieForm

---
+++ src/components/serieForm.jsx
@@ -0,0 +1,12 @@
+export default function serieForm() {
+
+    return (
+        <>
+        <form>
+            <input type="number" />
+            <button>Calcular</button>
+        </form>
+        </>
+        
+    )
+  }
